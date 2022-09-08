10fbc10b8040112fd2f7c926229312a28e93b484
Fusss <30801524+fusss9@users.noreply.github.com>
2022-09-08 23:37:45 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,33 +1,4 @@
 export const config = {
-  /**
-   * 公众号配置
-   */
-
-  // 公众号APP_ID 
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
-  // APP_ID: "",
-
-  // 公众号APP_SECRET 
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
-  // APP_SECRET: "",
-
-  /**
-   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
-   * [
-   *  {
-   *    // 想要发送的人的名字
-   *    name: "老婆0",
-   *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
-   *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
-   *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *    openUrl: "https://wangxinleo.cn"
-   *   }, 
-   * ]
-   */
   USERS: [
     {
       // 想要发送的人的名字
@@ -35,7 +6,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oKyBR5_lRMRspTA897bLvzf91ocs",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "_jFMUbtrdSL5M8iHz2dwCEZnd97Qze8CRywD9xdelZ0",
+      useTemplateId: "AtGKJsgnhkl1vpdEbFJ1kv3g7KTnk_Y8OpeMgdVygaU",
       // 所在省份
       province: "湖南",
       // 所在城市
@@ -46,27 +17,11 @@ export const config = {
     {
       name: "测试", 
       id: "oKyBR56k-rhjK3X-fdetGTVfijEM",
-      useTemplateId: "_jFMUbtrdSL5M8iHz2dwCEZnd97Qze8CRywD9xdelZ0",
+      useTemplateId: "AtGKJsgnhkl1vpdEbFJ1kv3g7KTnk_Y8OpeMgdVygaU",
       province: "湖南",
       city: "永州",
       openUrl: "https://v1.jinrishici.com/rensheng.txt"
     }, 
-//     {
-//       name: "老婆2",
-//       id: "",
-//       useTemplateId: "",
-//       province: "",
-//       city: "",
-//       openUrl: "https://wangxinleo.cn"
-//     }, 
-//     {
-//       name: "老婆3", 
-//       id: "",
-//       useTemplateId: "",
-//       province: "",
-//       city: "",
-//       openUrl: "https://wangxinleo.cn"
-//     }, 
   ],
 
   /**
@@ -106,10 +61,8 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "美丽可爱亲亲老婆", "year": "1999", "date": "10-31"},
-   // {"type": "节日", "name": "纪念日", "year": "2021", "date": "09-03"},
+    {"type": "生日", "name": "乖乖", "year": "1999", "date": "10-31"},
      {"type": "生日", "name": "帆帆", "year": "2001", "date": "03-11"},
-//     {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
   ],
 
   /**
@@ -126,15 +79,8 @@ export const config = {
     * 比如：keyword: "love_date" ，在测试号中就是 {{ love_date.DATA }} 
     * */
   CUSTOMIZED_DATE_LIST: [
-    // 在一起的日子
     {"keyword": "love_day", date: "2021-01-26"},
-    // 结婚纪念日
-  //  {"keyword": "marry_day", date: "2020-01-04"},
-    // 退伍日, 不用可以删掉
-    {"keyword": "ex_day", date: "2024-09-01"},
-    // sakana日
-    // {"keyword": "sakana_day", date: "2022-01-06"},
-    // ... 
+    {"keyword": "tw_day", date: "2024-09-01"},
   ],
 
   /** 插槽 */
