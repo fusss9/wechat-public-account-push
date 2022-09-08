7ed7b37d2fd5457f61ae636d3670a86ff25131f5
Fusss <30801524+fusss9@users.noreply.github.com>
2022-09-08 22:48:14 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,17 +31,17 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "老婆",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      id: "oKyBR5_lRMRspTA897bLvzf91ocs",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      useTemplateId: "95FvEbHmp6n2MBoQoIJVP8MHkBNeSiE1-1pNOrKeUIU",
       // 所在省份
-      province: "广东",
+      province: "湖南",
       // 所在城市
-      city: "肇庆",
+      city: "永州",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "https://v1.jinrishici.com/rensheng.txt"
     }, 
     {
       name: "老婆1", 
@@ -93,9 +93,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "湖南",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "永州",
 
   /** 重要节日相关 */
 
@@ -106,10 +106,10 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+    {"type": "生日", "name": "美丽可爱亲亲老婆", "year": "1999", "date": "10-31"},
+   // {"type": "节日", "name": "纪念日", "year": "2021", "date": "09-03"},
+//     {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
+//     {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
   ],
 
   /**
@@ -127,11 +127,11 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2015-05-01"},
+    {"keyword": "love_day", date: "2021-01-26"},
     // 结婚纪念日
-    {"keyword": "marry_day", date: "2020-01-04"},
+  //  {"keyword": "marry_day", date: "2020-01-04"},
     // 退伍日, 不用可以删掉
-    {"keyword": "ex_day", date: "2022-08-31"},
+    {"keyword": "ex_day", date: "2024-09-01"},
     // sakana日
     // {"keyword": "sakana_day", date: "2022-01-06"},
     // ... 
@@ -145,13 +145,15 @@ export const config = {
     * */
   SLOT_LIST: [
     // 这样配置的话，就会每次发送这句话
-    {"keyword": "encourage_oneself", contents: "你主要的问题在于读书太少而想得太多"},
+    //{"keyword": "encourage_oneself", contents: "你主要的问题在于读书太少而想得太多"},
     // 这样配置的话，就会每次随机选一句话发送
     {"keyword": "lover_prattle", contents: [
-      "因为太喜欢你，所以看谁都像是情敌。",
-      "申请成为你爱里的永久居民。",
-      "你很傻，你很笨，可我还是很羡慕你，因为你有我",
-      "遇见你，就好像捡到了100斤的运气",
+      "加油宝宝，相信自己是！！姐就是女王，怕他她它啥，该做就去做大胆去做（正确的）。",
+      "别健忘！上车下车记得摸摸口袋，看清楚东西！！",
+      "如果一个人在外，要保护好自己！",
+      "乖，我真的好爱好爱你啊",
+      "如果你想我了，就抬头看看月亮，我也在想你",
+      "每天站岗的时候我都在想你",
     ]},
   ],
 
