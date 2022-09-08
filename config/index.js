04232634e1a104b612a049a178193a22a1b20d27
Fusss <30801524+fusss9@users.noreply.github.com>
2022-09-08 22:59:16 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -43,30 +43,30 @@ export const config = {
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://v1.jinrishici.com/rensheng.txt"
     }, 
-    {
-      name: "老婆1", 
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      openUrl: "https://wangxinleo.cn"
-    }, 
-    {
-      name: "老婆2",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      openUrl: "https://wangxinleo.cn"
-    }, 
-    {
-      name: "老婆3", 
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      openUrl: "https://wangxinleo.cn"
-    }, 
+//     {
+//       name: "老婆1", 
+//       id: "",
+//       useTemplateId: "",
+//       province: "",
+//       city: "",
+//       openUrl: "https://wangxinleo.cn"
+//     }, 
+//     {
+//       name: "老婆2",
+//       id: "",
+//       useTemplateId: "",
+//       province: "",
+//       city: "",
+//       openUrl: "https://wangxinleo.cn"
+//     }, 
+//     {
+//       name: "老婆3", 
+//       id: "",
+//       useTemplateId: "",
+//       province: "",
+//       city: "",
+//       openUrl: "https://wangxinleo.cn"
+//     }, 
   ],
 
   /**
@@ -74,7 +74,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "zjV1glIuCXo4Csr23aXWEio1iP74sZJ7IQEES95lDsg6c4yOU",
+  CALLBACK_TEMPLATE_ID: "86az3gc-BmHzRDqgQ-X1EFM0xTMyBLdWavKFan3aBR8",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -82,7 +82,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
+      id: "oKyBR56k-rhjK3X-fdetGTVfijEM",
     }, 
   ],
     
